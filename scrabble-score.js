3977cf8d29a16df1b3a99b3f1d2395eb8083ea85
Amin Abdikadir <amino@Amins-MacBook-Pro.local>
2024-08-06 15:20:30 +0100

    ticket 2,3,4

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,3 +1,43 @@
+// scrabble-score.js
+// Plan:
+// 1. Use split("") on a string to break the word into an array of individual letters.
+// 2. Create a loop that goes through each letter one by one.
+// 3. For each letter, find out how many points it is worth using the pointsTable above.
+// 4. Create a score variable and add the points for each letter to this score variable.
+export const letterScores = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
 export function calculateScrabbleScore(word) {
-  if (word === "A") return 1;
+  const letters = word.split("");
+  let score = 0;
+  letters.forEach(function (letter) {
+    const letterPoints = letterScores[letter];
+    score = score + letterPoints;
+  });
+  return score;
 }
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,6 +1,15 @@
 import { test, expect } from "vitest";
-import { calculateScrabbleScore } from "./scrabble-score";
+import { calculateScrabbleScore , letterScores } from "./scrabble-score"; 
+const alphabet = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
 
 test("score of a single letter", () => {
   expect(calculateScrabbleScore("A")).toEqual(1);
+});  
+test('score of each letter', () => {
+  alphabet.forEach(letter => {
+   expect(calculateScrabbleScore(letter)).toBe(letterScores[letter]) 
+  });
 });
