81e03c42a02958397e54800ec7eb6335c9939a6f
Mahik <mahik09@outlook.com>
2024-08-06 13:41:13 +0100

    ticket 1 and first part of ticket 2 complete

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,6 @@
+import { test, expect } from "vitest";
 import { calculateScrabbleScore } from "./scrabble-score";
+
+test("score of a single letter", () => {
+  expect(calculateScrabbleScore("A")).toEqual(1);
+});
